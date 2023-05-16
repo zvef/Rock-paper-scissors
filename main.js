1182c104a8cd57435a4af19e165fdd1551663a84
zvef <jakekneece7@gmail.com>
2023-05-15 20:09:31 -0400

    Added playRound functionm with tests for it

--- main.js
+++ main.js
@@ -7,16 +7,32 @@ number = Math.floor(Math.random()*3);
 
 function getComputerChoice() {
     if(number == 0){
-        console.log(rock);
+        return rock;
     }
     else if(number == 1){
-        console.log(paper);
+        return paper;
     }
     else {
-        console.log(scissors);
+        return scissors;
     }
 }
-   
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == rock && computerSelection == paper) {
+        return "You Lose! Paper beats Rock"
+    }
+    else if (playerSelection == paper && computerSelection == scissors) {
+        return "You Lose! Scissors beats Paper"
+    }
+    else if (playerSelection == scissors && computerSelection == rock) {
+        return "You Lose! Rock beats Scissors"
+    }
+    else {
+        return "You Win!"
+    }
+}
+
+const playerSelection = paper;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
