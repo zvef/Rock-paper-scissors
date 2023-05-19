b86224c75660d231e25c0b801919809281267064
zvef <jakekneece7@gmail.com>
2023-05-19 11:47:18 -0400

    ADDED BUTTONS each option has a working button for input

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-
+//STRING VARIABLES
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
+
+//WIN MESSAGES
 const playerWmsg = "You Win!";
 const computerWmsg = "You Lose!";
 let number = Math.floor(Math.random()*3);
 
+//BUTTON VARIABLES
+const rockButton = document.querySelector('#butt1');
+const paperButton = document.querySelector('#butt2');
+const scissorsButton = document.querySelector('#butt3');
+
+//COMPUTER SELECTION VARIABLE
+const computerSelection = getComputerChoice();
+
+//SCORE DISPLAY VARIABLES
+let playerScoreDisplay = document.querySelector('.result');
+let computerScoreDisplay = document.querySelector('.result2');
+
+//TEST SCORE VARIABLES
+let playerScore = 0;
+let computerScore = 0;
+
+//BUTTON CLICKS
+rockButton.onclick = () => {
+    playRound(rock, computerSelection);
+}
+paperButton.onclick = () => {
+    playRound(paper, computerSelection);
+}
+scissorsButton.onclick = () => {
+    playRound(scissors, computerSelection);
+}
+
+
+//GAME FUNCTIONS
 function getComputerChoice() {
     if(number == 0){
         return rock;
@@ -17,29 +48,29 @@ function getComputerChoice() {
         return scissors;
     }
 }
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == rock && computerSelection == paper) {
-        return 1
+        // return 1
+        updateComputerScore();
     }
     else if (playerSelection == paper && computerSelection == scissors) {
-        return 1
+        // return 1
+        updateComputerScore();
     }
     else if (playerSelection == scissors && computerSelection == rock) {
-        return 1
+        // return 1
+        updateComputerScore();
     }
     else {
-        return 0
+        // return 0
+        updatePlayerScore();
     }
 }
-
-function game(number) {
+function game() {
     let computerWins = 0;
     let playerWins = 0;
-    let turnCount = number;
     const computerSelection = getComputerChoice();
-    for(let i = 0; i < number; i ++) {
-        const playerSelection = prompt("Enter rock, paper, or scissors (times: " + turnCount + ")");
+    for(let i = 0; i < turns; i ++) {
         playRound(playerSelection, computerSelection);
         if (playRound(playerSelection, computerSelection) == 1) {
             computerWins ++;
@@ -47,7 +78,6 @@ function game(number) {
         else {
             playerWins ++;
         }
-        turnCount --;
     }
 
     if (computerWins > playerWins) {
@@ -61,4 +91,13 @@ function game(number) {
     }
 }
 
-game(3);
+//game(); //runs game
+
+//DISPLAY COUNTER FUNCTION
+function updateComputerScore(){
+    computerScoreDisplay.innerHTML = computerScore++;
+};
+
+function updatePlayerScore() {
+    playerScoreDisplay.innerHTML = playerScore++;
+}
